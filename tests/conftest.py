1790d73df37ecd5e113289a7eaa2079df73adee9
Zonotora <Zonotora@hotmail.com>
2022-07-12 18:17:22 +0200

    Add perturbed dataset with carbon intensities

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,16 +8,15 @@ from scheduler.constants import REGION_NAMES, LOCATIONS
 
 
 @pytest.fixture
-def df_carbon_intensity_0_24():
-    return pd.read_csv("data/carbon_intensity_avg_0_24.csv")
+def df_carbon_intensity():
+    return pd.read_csv("data/carbon_intensity_avg.csv")
 
 
 @pytest.fixture
-def servers_0_24():
+def servers_infty():
     servers = []
-    df_all = pd.read_csv("data/carbon_intensity_avg_0_24.csv")
-    names = ["CAL", "MIDA", "MIDW", "TEX"]
-    for name, location in zip(names, LOCATIONS):
+    df_all = pd.read_csv("data/carbon_intensity_avg.csv")
+    for name, location in zip(df_all.columns, LOCATIONS):
         df = pd.DataFrame(data={"carbon_intensity_avg": df_all[name]})
         r = Region(name, location)
         s = Server(999999, r, df)
--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -7,12 +7,12 @@ from scheduler.region import Region
 from scheduler.constants import REGION_NAMES, LOCATIONS
 
 
-def test_carbon_greedy_day_0_24(df_carbon_intensity_0_24, servers_0_24):
+def test_carbon_greedy_day_0_24(df_carbon_intensity, servers_infty):
     max_time = 24
-    scheduler = Scheduler(servers_0_24)
+    scheduler = Scheduler(servers_infty, scheduler="carbon_greedy")
     region = Region(REGION_NAMES[0], LOCATIONS[0])
 
     for dt in range(max_time):
         task = TaskBatch("Task 1", 1, 1, region)
         d = scheduler.schedule([task], dt)
-        assert d["carbon_intensity"] == df_carbon_intensity_0_24.iloc[dt].min()
+        assert d["carbon_intensity"] == df_carbon_intensity[0:24].iloc[dt].min()
