c49de2565e5a148a718d29e5eac47a4cf039f71f
fiskrt <filipskogh99@hotmail.com>
2022-07-13 13:12:13 -0400

    added carbon-latency-capacity LP

--- scheduler/lp_sched.py
+++ scheduler/lp_sched.py
@@ -1,5 +1,6 @@
 from scipy.optimize import linprog
 import numpy as np
+import math
 
 
 def sched_only_carbon():
@@ -37,29 +38,39 @@ def sched_carb_latency():
         mu is the unit dependent parameter that
         trade-offs carbon and latency. 
     """
-    mu = 0.5 
+    mu = 1.2 
 
-
-    n_servers = 10
-    carb_intensity = list(range(1,n_servers+1))
-    latency = list(range(1,n_servers+1))
-    c = carb_intensity + latency
-    capacities = [50]*n_servers
+    #carb_intensity = np.arange(1, n_servers+1) 
+    #latency = np.arange(1, n_servers+1) 
+    carb_intensity = [2,3,5,5,5]
+    latency = np.asarray([5,5,5,3,1])
+    obj_coef = np.concatenate([carb_intensity, mu*latency])
     num_reqs = 100
+    n_servers = len(latency) 
 
+    # Each server can take [0,inf] tasks
+    bnd = [(0, float("inf"))]*(2*n_servers)
 
-    obj = carb_intensity
-    bnd = [(0, float("inf"))]*n_servers
-    lhs_ineq = np.identity(n_servers)
+    # Capacity constraint. Server i has capacity capacities[i] 
+    cap = 50
+    capacities = [cap]*n_servers 
+    lhs_ineq = np.concatenate([np.identity(n_servers), np.zeros((n_servers,n_servers))], axis=1)
     rhs_ineq = capacities
 
+    # Make sure all tasks are scheduled somewhere.
+    lhs_eq = np.zeros((n_servers, 2*n_servers))
+    lhs_eq[0, :n_servers] = 1
+    rhs_eq = [num_reqs] + [0]*(n_servers-1) + [0]*n_servers
 
-    lhs_eq = np.zeros((n_servers, n_servers))
-    lhs_eq[0] = 1
-    rhs_eq = [num_reqs] + [0]*(n_servers-1)
+    lhs_eq2 = np.concatenate([np.eye(n_servers), -np.eye(n_servers)], axis=1)
+    lhs_eq = np.concatenate([lhs_eq, lhs_eq2])
 
-    opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq,
+    opt = linprog(c=obj_coef, A_ub=lhs_ineq, b_ub=rhs_ineq,
                 A_eq=lhs_eq, b_eq=rhs_eq, bounds=bnd,
                 method="revised simplex")
+    print(opt.message)
+    print(f'Objective value: {opt.fun}')
+    return [math.ceil(x) for x in opt.x][:n_servers]
 
-    return [math.ceil(x) for x in opt.x]
+a = sched_carb_latency()
+print(a)
