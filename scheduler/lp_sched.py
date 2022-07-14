6aa74de3ae99a4119ddfbba0d954c23e28ea5a0a
fiskrt <filipskogh99@hotmail.com>
2022-07-14 11:58:22 -0400

    LP sched improvments 2

--- scheduler/lp_sched.py
+++ scheduler/lp_sched.py
@@ -9,6 +9,7 @@ def sched_only_carbon():
         at respective server. Ex: 5 requests to server 1, 10 to 
         server 2 and none to server 3 would be [5,10,0]
     """
+    raise NotImplementedError
     n_servers = 10
     carb_intensity = list(range(1,n_servers+1))
     capacities = [50]*n_servers
@@ -31,36 +32,47 @@ def sched_only_carbon():
 
     return [math.ceil(x) for x in opt.x]
 
-def sched_carb_latency():
+def schedule(n_tasks):
     """
-        Considers carbon, latency and capacity.
+    """
+    # In some way (api/database) get the servers' carbon,latency, capacity
+    carb_intensity = [2,3,5,5,5]
+    latency = np.asarray([5,5,5,3,1])
+    cap = 50
+    capacities = [cap]*len(latency)
+    return sched_carb_latency(n_tasks, carb_intensity, capacities, latency)
+
+
+def sched_carb_latency(n_tasks, carb_intensity, capacities, latency, mu=1):
+    """
+        Takes a number of fungible tasks from a location, and 
+        schedules them on servers 1,...,n while considering
+        the carbon intensity I_i, latency tau_i and capacity c_i
+        of each server i.
+
+
+        Input: 
+        The number of tasks to be scheduled.
 
         mu is the unit dependent parameter that
         trade-offs carbon and latency. 
     """
-    mu = 1.2 
-
-    #carb_intensity = np.arange(1, n_servers+1) 
-    #latency = np.arange(1, n_servers+1) 
-    carb_intensity = [2,3,5,5,5]
-    latency = np.asarray([5,5,5,3,1])
-    obj_coef = np.concatenate([carb_intensity, mu*latency])
-    num_reqs = 100
     n_servers = len(latency) 
+
+    # The objective function's coefficients
+    obj_coef = np.concatenate([carb_intensity, mu*latency])
 
     # Each server can take [0,inf] tasks
     bnd = [(0, float("inf"))]*(2*n_servers)
 
     # Capacity constraint. Server i has capacity capacities[i] 
-    cap = 50
-    capacities = [cap]*n_servers 
     lhs_ineq = np.concatenate([np.identity(n_servers), np.zeros((n_servers,n_servers))], axis=1)
     rhs_ineq = capacities
 
     # Make sure all tasks are scheduled somewhere.
     lhs_eq = np.zeros((n_servers, 2*n_servers))
     lhs_eq[0, :n_servers] = 1
-    rhs_eq = [num_reqs] + [0]*(n_servers-1) + [0]*n_servers
+    rhs_eq = [n_tasks] + [0]*(n_servers-1) + [0]*n_servers
 
     lhs_eq2 = np.concatenate([np.eye(n_servers), -np.eye(n_servers)], axis=1)
     lhs_eq = np.concatenate([lhs_eq, lhs_eq2])
@@ -72,5 +84,5 @@ def sched_carb_latency():
     print(f'Objective value: {opt.fun}')
     return [math.ceil(x) for x in opt.x][:n_servers]
 
-a = sched_carb_latency()
+a = schedule(n_tasks=100)
 print(a)
