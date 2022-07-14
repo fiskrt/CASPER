64665d4353cd878a63da8f583258d38991ab2840
Zonotora <Zonotora@hotmail.com>
2022-07-14 21:48:03 +0200

    Plot utilization for all and each server

--- scheduler/plot.py
+++ scheduler/plot.py
@@ -23,8 +23,8 @@ class Plot:
 
         self.data[dt].append(data)
 
-    def __preprocess(self, key: str, test=True):
-        if test:
+    def __preprocess(self, key: str, all=True):
+        if all:
             mean = np.array([np.mean(list(map(lambda y: y[key], x))) for x in self.data])
             std = np.array([np.std(list(map(lambda y: y[key], x))) for x in self.data])
         else:
@@ -47,12 +47,38 @@ class Plot:
 
         return mean, std
 
+    def __preprocess_server(self, key: str, all=True):
+        if all:
+            mean = np.array([np.mean(list(map(lambda y: y["server"][key], x))) for x in self.data])
+            std = np.array([np.std(list(map(lambda y: y["server"][key], x))) for x in self.data])
+        else:
+            values_with_names = [list(map(lambda y: (y["server"][key], y["server"]["name"]), x)) for x in self.data]
+            mean = np.zeros(shape=(len(values_with_names), len(REGION_NAMES)))
+            std = np.zeros(shape=(len(values_with_names), len(REGION_NAMES)))
+            for i, item in enumerate(values_with_names):
+                values = {name: [] for name in REGION_NAMES}
+                for (v, name) in item:
+                    values[name].append(v)
+                for name in REGION_NAMES:
+                    values[name] = (np.mean(values[name]), np.std(values[name]))
+
+                for j in range(len(REGION_NAMES)):
+                    name = REGION_NAMES[j]
+                    mean[i, j] = values[name][0]
+                    std[i, j] = values[name][1]
+            mean = pd.DataFrame(data=mean, columns=REGION_NAMES)
+            std = pd.DataFrame(data=std, columns=REGION_NAMES)
+
+        return mean, std
+
     def plot(self, conf):
         mean_latency, std_latency = self.__preprocess("latency")
         mean_carbon_intensity, std_carbon_intensity = self.__preprocess("carbon_intensity")
+        mean_utilization, std_utilization = self.__preprocess_server("utilization")
         graphs = {
             "latency": [mean_latency, std_latency],
             "carbon_intensity": [mean_carbon_intensity, std_carbon_intensity],
+            "utilization": [mean_utilization, std_utilization],
         }
 
         fig = plt.figure(figsize=(18, 14))
@@ -63,7 +89,7 @@ class Plot:
             [mean, std] = graphs[key]
             error = 1.96 * std / np.sqrt(len(x))
 
-            ax = plt.subplot(2, 2, i + 1)
+            ax = plt.subplot(2, 3, i + 1)
             ax.set_title(key)
             ax.plot(x, mean, label=key)
             ax.fill_between(x, mean - error, mean + error, alpha=0.3)
@@ -72,15 +98,17 @@ class Plot:
 
         mean_servers_latency, std_servers_latency = self.__preprocess("latency", False)
         mean_servers_carbon_intensity, std_servers_carbon_intensity = self.__preprocess("carbon_intensity", False)
+        mean_servers_utilization, std_servers_utilization = self.__preprocess_server("utilization", False)
         graphs_servers = {
             "latency": [mean_servers_latency, std_servers_latency],
             "carbon_intensity": [mean_servers_carbon_intensity, std_servers_carbon_intensity],
+            "utilization": [mean_servers_utilization, std_servers_utilization],
         }
         for key in graphs_servers:
             [mean, std] = graphs_servers[key]
             error = 1.96 * std / np.sqrt(len(x))
 
-            ax = plt.subplot(2, 2, i + 1)
+            ax = plt.subplot(2, 3, i + 1)
             ax.set_title(key)
             mean.plot(kind="line", ax=ax)
             i += 1
