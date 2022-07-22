52550cd431269f281f7520a2c68fbbf7bca70087
fiskrt <filipskogh99@hotmail.com>
2022-07-22 17:24:17 -0400

    fixed return of schedulers

--- scheduler/milp_sched.py
+++ scheduler/milp_sched.py
@@ -92,10 +92,13 @@ def place_servers(req_rates, capacities, latencies, carb_intensities, max_server
                             for j in set_R)
     opt_model.setObjective(objective) 
     opt_model.solve()
+    if opt_model.sol_status != 1:
+        print('Did not find sol!')
 
-    # TODO: fix ordering of vals 
-    return [(s.name, s.varValue) for s in opt_model.s_vars]
+    return [int(s.varValue) for s in s_vars.values()]
 
+#a = place_servers([10,10,11]*10, [10,10,10]*10, [[i for i in range(30)] for _ in range(30)]*10, [100, 100, 100]*10, 56, 20)
+#print(a)
 
 def sched_reqs(req_rates, capacities, latencies, carb_intensities, servers, max_latency):
     """
@@ -144,54 +147,12 @@ def sched_reqs(req_rates, capacities, latencies, carb_intensities, servers, max_
                             for j in set_R)
     opt_model.setObjective(objective) 
     opt_model.solve()
-    for v in opt_model.variables():
-        if 's' in v.name:
-            print(v.name, "=", v.varValue)
-
-a = place_servers([10,10,11]*10, [10,10,10]*10, [[i for i in range(30)] for _ in range(30)]*10, [100, 100, 100]*10, 56, 20)
-print(a)
-
-def sched_carb_latency(n_tasks, carb_intensity, capacities, latency, mu=1):
-    """
-    Takes a number of fungible tasks from a location, and
-    schedules them on servers 1,...,n while considering
-    the carbon intensity I_i, latency tau_i and capacity c_i
-    of each server i.
-
-    Example: 5 requests to server 1, 10 to
-    server 2 and none to server 3 would be [5,10,0]
-
-    Input:
-    The number of tasks to be scheduled.
-
-    mu is the unit dependent parameter that
-    trade-offs carbon and latency.
-    """
-    n_servers = len(latency)
-
-    # The objective function's coefficients
-    obj_coef = np.concatenate([carb_intensity, mu * latency])
-
-    # Each server can take [0,inf] tasks
-    bnd = [(0, float("inf"))] * (2 * n_servers)
-
-    # Capacity constraint. Server i has capacity capacities[i]
-    lhs_ineq = np.concatenate([np.identity(n_servers), np.zeros((n_servers, n_servers))], axis=1)
-    rhs_ineq = capacities
-
-    # Make sure all tasks are scheduled somewhere.
-    lhs_eq = np.zeros((n_servers, 2 * n_servers))
-    lhs_eq[0, :n_servers] = 1
-    rhs_eq = [n_tasks] + [0] * (n_servers - 1) + [0] * n_servers
-
-    lhs_eq2 = np.concatenate([np.eye(n_servers), -np.eye(n_servers)], axis=1)
-    lhs_eq = np.concatenate([lhs_eq, lhs_eq2])
-
-    opt = linprog(
-        c=obj_coef, A_ub=lhs_ineq, b_ub=rhs_ineq, A_eq=lhs_eq, b_eq=rhs_eq, bounds=bnd, method="revised simplex"
-    )
-    if opt.status == 2:
-        print("Warning: No server availible!")
-    # print(opt.message)
-    # print(f"Objective value: {opt.fun}")
-    return np.asarray([math.ceil(x) for x in opt.x][:n_servers])
+    if opt_model.sol_status != 1:
+        print('Did not find sol!')
+    requests = np.zeros((len(set_R), len(set_R)))
+    for i,j in x_vars.keys():
+        requests[i,j]= x_vars[i,j].varValue
+    return requests
+#a = sched_reqs([10,10,11]*10, [10,10,10]*10, [[i for i in range(30)] for _ in range(30)]*10,
+             [100, 100, 100]*10, [10,10,10]*10, 20)
+#print(a)
